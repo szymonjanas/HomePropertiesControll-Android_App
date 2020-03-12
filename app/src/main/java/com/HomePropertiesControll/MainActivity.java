e8d5ba7d98c6b9247def2db516b6ef3e07b909f9
szymon <szymon.janas.95@gmail.com>
2020-03-12 02:49:50 +0100

    Refactor: changed mainActivity after export sensorsList

--- app/src/main/java/com/HomePropertiesControll/MainActivity.java
+++ app/src/main/java/com/HomePropertiesControll/MainActivity.java
@@ -1,113 +1,96 @@
 package com.HomePropertiesControll;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.HomePropertiesControll.HttpRequest.HttpRequestSingleton;
-import com.HomePropertiesControll.ItemsList.Sensor;
-import com.HomePropertiesControll.ItemsList.SensorAdapter;
+import com.HomePropertiesControll.HttpRequest.HttpUserRequest;
+import com.HomePropertiesControll.HttpRequest.OnUserResponseCallback;
+import com.HomePropertiesControll.Sensors.SensorsList;
 import com.HomePropertiesControll.User.User;
 import com.HomePropertiesControll.User.UserModel;
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView sensorRecycleView;
-    private RecyclerView.Adapter sensorAdapter;
-    private RecyclerView.LayoutManager sensorLayoutManager;
-    final ArrayList<Sensor> sensorArrayList = new ArrayList<>();
     final Context context = this;
+    private Button loginBtn;
+    private EditText username;
+    private EditText password;
+    private TextView errorMessage;
+    final UserModel user = new UserModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
+        loginBtn = findViewById(R.id.loginBtn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        errorMessage = findViewById(R.id.errorMessage);
 
-        User.initInstance();
-        User.getInstance().setUser(new UserModel("android", "androidpassword"));
-        final Button refreshButton = findViewById(R.id.refreshButton);
-
-        sensorRecycleView = findViewById(R.id.sensors_recycler_viewer);
-        sensorRecycleView.setHasFixedSize(true);
-        sensorLayoutManager = new LinearLayoutManager(this);
-        sensorAdapter = new SensorAdapter(sensorArrayList);
-        sensorRecycleView.setLayoutManager(sensorLayoutManager);
-        sensorRecycleView.setAdapter(sensorAdapter);
-
-        refreshButton.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpRequestSingleton.getInstance(context).addToRequestQueue(refresh());
+                Log.i("BTN", "HELLO");
+                isLogin();
             }
         });
-
-
     }
 
-    public JsonArrayRequest refresh(){
-        sensorArrayList.clear();
-        String url ="http://10.0.2.2:8080/api/android";
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        sensorArrayList.clear();
-                        for (int i = 0; i < response.length(); i++) {
-                            JSONObject jsonobject = null;
-                            try {
-                                jsonobject = response.getJSONObject(i);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            try {
-                                sensorArrayList.add(new Sensor(
-                                        jsonobject.getString("id"),
-                                        jsonobject.getString("name"),
-                                        jsonobject.getString("type"),
-                                        jsonobject.getString("location"),
-                                        jsonobject.getInt("level")
-                                ));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            sensorAdapter.notifyDataSetChanged();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        sensorArrayList.clear();
-                        sensorAdapter.notifyDataSetChanged();
-                        System.out.println(error.toString());
-                    }
-                }){
+    public void onClick(final UserModel user){
+         HttpUserRequest userRequest = new HttpUserRequest(new OnUserResponseCallback() {
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> params = new HashMap<String, String>();
-                params.put("Authorization", User.getInstance().getUserAuthorization());
-                return params;
+            public void onResponse(String response) {
+                if (response.equals("TRUE")){
+                    Log.i("USER", "Good user");
+                        user.setLogin(true);
+                        User.getInstance().setUser(user);
+                        showSensorList();
+                } else {
+                    setNotLogin("Invalid user or password!");
+                }
             }
-        };
-        return jsonArrayRequest;
+            @Override
+            public void onError(VolleyError error) {
+                setNotLogin("Server error:" + error.toString());
+            }});
+        userRequest.sendRequest(context, user);
+    }
+
+    public void showSensorList(){
+        Intent intent = new Intent(this, SensorsList.class);
+        startActivity(intent);
+        Log.i("SHOW", "test");
+    }
+
+    public void setNotLogin(String message){
+        errorMessage.setText(message);
+    }
+
+    public void isLogin(){
+        user.setUsername(username.getText().toString());
+        user.setPassword(password.getText().toString());
+        Log.i("USERNAME", user.getUsername());
+        Log.i("PASSWORD", user.getPassword());
+        Log.i("BTN", " HELLO 2");
+        onClick(user);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (user.isLogin()){
+            showSensorList();
+        }
     }
 }
