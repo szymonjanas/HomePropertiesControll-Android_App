ab1f30824e5a6b63fb0de75866e12cfafd86a8ba
szymon <szymon.janas.95@gmail.com>
2020-03-13 19:36:48 +0100

    Add, Refactor: login, remember me,
    export login to separate activity

--- app/src/main/java/com/HomePropertiesControll/MainActivity.java
+++ app/src/main/java/com/HomePropertiesControll/MainActivity.java
@@ -1,96 +1,31 @@
 package com.HomePropertiesControll;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.HomePropertiesControll.HttpRequest.HttpUserRequest;
-import com.HomePropertiesControll.HttpRequest.OnUserResponseCallback;
-import com.HomePropertiesControll.Sensors.SensorsList;
-import com.HomePropertiesControll.User.User;
-import com.HomePropertiesControll.User.UserModel;
-import com.android.volley.VolleyError;
+import com.HomePropertiesControll.HttpRequest.HttpRequestSingleton;
+import com.HomePropertiesControll.User.LoginActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    final Context context = this;
-    private Button loginBtn;
-    private EditText username;
-    private EditText password;
-    private TextView errorMessage;
-    final UserModel user = new UserModel();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-        loginBtn = findViewById(R.id.loginBtn);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        errorMessage = findViewById(R.id.errorMessage);
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("BTN", "HELLO");
-                isLogin();
-            }
-        });
+        HttpRequestSingleton.initHttpRequestSingleton(this);
+        login();
     }
 
-    public void onClick(final UserModel user){
-         HttpUserRequest userRequest = new HttpUserRequest(new OnUserResponseCallback() {
-            @Override
-            public void onResponse(String response) {
-                if (response.equals("TRUE")){
-                    Log.i("USER", "Good user");
-                        user.setLogin(true);
-                        User.getInstance().setUser(user);
-                        showSensorList();
-                } else {
-                    setNotLogin("Invalid user or password!");
-                }
-            }
-            @Override
-            public void onError(VolleyError error) {
-                setNotLogin("Server error:" + error.toString());
-            }});
-        userRequest.sendRequest(context, user);
-    }
-
-    public void showSensorList(){
-        Intent intent = new Intent(this, SensorsList.class);
+    public void login(){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-        Log.i("SHOW", "test");
-    }
-
-    public void setNotLogin(String message){
-        errorMessage.setText(message);
-    }
-
-    public void isLogin(){
-        user.setUsername(username.getText().toString());
-        user.setPassword(password.getText().toString());
-        Log.i("USERNAME", user.getUsername());
-        Log.i("PASSWORD", user.getPassword());
-        Log.i("BTN", " HELLO 2");
-        onClick(user);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (user.isLogin()){
-            showSensorList();
-        }
+        login();
     }
 }
---
+++ app/src/main/java/com/HomePropertiesControll/User/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.HomePropertiesControll.User;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.HomePropertiesControll.HttpRequest.HttpRequestSingleton;
+import com.HomePropertiesControll.HttpRequest.HttpUserRequest;
+import com.HomePropertiesControll.HttpRequest.OnUserResponseCallback;
+import com.HomePropertiesControll.R;
+import com.HomePropertiesControll.Sensors.SensorsListActivity;
+import com.android.volley.VolleyError;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private TextView errorMessage;
+    private CheckBox rememberMe;
+    final UserModel user = new UserModel();
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+
+    @SuppressLint("CommitPrefEdits")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        errorMessage = findViewById(R.id.errorMessage);
+        rememberMe = findViewById(R.id.remember_me);
+
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin.equals(true)){
+            username.setText(loginPreferences.getString("username", ""));
+            password.setText(loginPreferences.getString("password", ""));
+            rememberMe.setChecked(true);
+        }
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("BTN", "HELLO");
+                isLogin();
+
+            }
+        });
+    }
+
+    public void onClick(final UserModel user){
+        HttpUserRequest userRequest = new HttpUserRequest(new OnUserResponseCallback() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("TRUE")){
+                    Log.i("USER", "Good user");
+                    user.setLogin(true);
+                    User.getInstance().setUser(user);
+                    if (!rememberMe.isChecked()){
+                        username.getText().clear();
+                        password.getText().clear();
+                    }
+                    showSensorList();
+                } else {
+                    setNotLogin("Invalid user or password!");
+                }
+            }
+            @Override
+            public void onError(VolleyError error) {
+                setNotLogin("Server error:" + error.toString());
+            }});
+        userRequest.sendRequest(user);
+    }
+
+    public void showSensorList(){
+        Intent intent = new Intent(this, SensorsListActivity.class);
+        startActivity(intent);
+        Log.i("SHOW", "test");
+    }
+
+    public void setNotLogin(String message){
+        errorMessage.setText(message);
+    }
+
+    public void isLogin(){
+        user.setUsername(username.getText().toString());
+        user.setPassword(password.getText().toString());
+        Log.i("USERNAME", user.getUsername());
+        Log.i("PASSWORD", user.getPassword());
+        Log.i("BTN", " HELLO 2");
+        if (rememberMe.isChecked()){
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            loginPrefsEditor.putString("username", user.getUsername());
+            loginPrefsEditor.putString("password", user.getPassword());
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
+        }
+
+        onClick(user);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
